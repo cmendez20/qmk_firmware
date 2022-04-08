d7d38f8b04f0255f3a95c4e7456004716f69fdca
Chris-Windows-Desktop <cmendez20@utexas.edu>
2022-04-07 20:33:07 -0500

    Fix function layer keycodes

--- keyboards/planck/keymaps/cmendez20/keymap.c
+++ keyboards/planck/keymaps/cmendez20/keymap.c
@@ -188,7 +188,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_FUN] = LAYOUT_planck_grid(
-    KC_F12,    KC_7,     KC_8,  KC_9,  KC_SLEP,   KC_5,  KC_NO, KC_NO,  KC_NO,     KC_NO,    KC_NO,     RESET,
+    KC_F12,    KC_F7,     KC_F8,  KC_F9,  KC_SLEP,   KC_5,  KC_NO, KC_NO,  KC_NO,     KC_NO,    KC_NO,     RESET,
     KC_F11,    KC_F4,    KC_F5, KC_F6, KC_WFWD,   KC_NO, KC_NO, KC_NO,  KC_RCTL,   KC_RSFT,  KC_LALT,   KC_RGUI,
     KC_F10,    KC_F1,    KC_F2, KC_F3, KC_WBAK,   KC_NO, KC_NO, KC_NO,  KC_NO,     KC_NO,    KC_NO,     KC_NO,
     KC_NO,     KC_NO,    KC_NO, KC_NO, KC_NO,     KC_NO, KC_NO, KC_NO,  KC_NO,     KC_TRNS,  KC_NO,     KC_NO
